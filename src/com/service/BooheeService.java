d5152795d99cf9b563aeac85061907b8b2de6ae2
xyy <xyy-JAVA@outlook.com>
2016-11-28 10:23:06 +0800

    使用jxl生成excel demo，处理部分数据

--- src/com/service/BooheeService.java
+++ src/com/service/BooheeService.java
@@ -11,21 +11,23 @@ import com.google.gson.Gson;
 import java.sql.Blob;
 import java.sql.ResultSet;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-//生成Execl所用类
-import  java.io.File;
-import  jxl.Workbook;
-import  jxl.write.Label;
-import  jxl.write.WritableSheet;
-import  jxl.write.WritableWorkbook;
+//生成Excel所用类
+import java.io.File;
+
+import jxl.Workbook;
+import jxl.write.Label;
+import jxl.write.WritableSheet;
+import jxl.write.WritableWorkbook;
 
 /**
  * BooheeService
  * com.service
  *
  * @author xiaoyy
- * @description
+ * @description java生成demo execl数据
  * @create 2016-11-25 上午10:07
  * The word 'impossible' is not in my dictionary.
  */
@@ -37,21 +39,22 @@ public class BooheeService {
 
     /**
      * 获取食品数据list，并进行gson转换对象处理
+     *
      * @return
      */
-    public static List<Result> booheeList(){
+    public List<Result> booheeList() {
         List<Boohee> booheeList = new ArrayList<>();
         List<Result> resultList = new ArrayList<>();
         String sql = "select * from boohee";
         db = new DBHelper(sql);
         try {
             rs = db.pst.executeQuery();
-            if (!StringHelper.isEmptyObject(rs)){
-                while (rs.next()){
+            if (!StringHelper.isEmptyObject(rs)) {
+                while (rs.next()) {
                     Boohee boohee = new Boohee();
                     boohee.setTaskid(rs.getString(1));
                     boohee.setUrl(rs.getString(2));
-                    if (rs.getBlob(3) instanceof Blob){
+                    if (rs.getBlob(3) instanceof Blob) {
                         Gson gson = new Gson();
                         Result result = gson.fromJson(rs.getString(3), Result.class);
                         boohee.setResult(result);
@@ -60,7 +63,7 @@ public class BooheeService {
                     booheeList.add(boohee);
                 }
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             System.out.print("查询数据出错" + e);
         }
         return resultList;
@@ -68,34 +71,49 @@ public class BooheeService {
 
 
     /**
-     * 生成处理Execl
+     * 生成处理Excel
+     *
      * @param resultList
      */
-    public static void CreateExcel(List<Result> resultList){
+    public void CreateExcel(List<Result> resultList) {
         try {
-            WritableWorkbook book = Workbook.createWorkbook(new File("text.xls"));
-            WritableSheet sheet = book.createSheet("乌拉拉" , 0);
-            for (int i = 0; i<resultList.size(); i++){
-                Label label  =   new  Label( 0 ,  i ,  resultList.get(i).getName() );
+            WritableWorkbook book = Workbook.createWorkbook(new File("牛掰的excel.xls"));
+            WritableSheet sheet = book.createSheet("乌拉拉", 0);
+            // 插入表头
+            for (int i = 0; i < Constants.title_row_all.length; i++) {
+                Label label = new Label(i, 0, Constants.title_row_all[i]);
+                sheet.addCell(label);
+            }
+
+            // 插入数据
+            for (int i = 0; i < resultList.size(); i++) {
+                Label label = new Label(0, i + 1, resultList.get(i).getName());
                 String contents = resultList.get(i).getContents();
                 String[] contentArr = contents.split(" ");
-                System.out.println(contentArr);
+                // 判断title是否存在
+                for (int j = 0; j < Constants.title_row.length; j++) {
+                    if (Arrays.asList(contentArr).contains(Constants.title_row[j])) {
+                        for (int k = 0; k < contentArr.length; k++) {
+                            if (contentArr[k].equals(Constants.title_row[j])) {
+                                Label label1 = new Label(j + 1, i + 1, contentArr[k + 1]);
+                                sheet.addCell(label1);
+                            }
+                        }
+                    } else {
+//                        Label label1 = new Label(j + 1, i + 1, "——");
+                        Label label1 = new Label(j + 1, i + 1, "一");
+                        sheet.addCell(label1);
+                    }
+                }
                 sheet.addCell(label);
             }
             //  写入数据并关闭文件
             book.write();
             book.close();
-        }catch (Exception e){
+        } catch (Exception e) {
             System.out.println("生成Excel出错" + e);
         }
 
-    }
-
-
-    public static void main(String[] args) {
-        List<Result> booheeList = booheeList();
-        System.out.println(booheeList);
-        CreateExcel(booheeList);
     }
 
 }
